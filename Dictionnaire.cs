25b30a8a37a7cd5ffdd64458a2f6ce4d37611306
MaloBardin <105814710+MaloBardin@users.noreply.github.com>
2023-11-27 12:53:23 +0100

    dico et joueur

--- Dictionnaire.cs
+++ Dictionnaire.cs
@@ -8,5 +8,102 @@ namespace ProjetInfoMotsCroises
 {
     internal class Dictionnaire
     {
+
+
+
+        public void Begin()
+        {
+            List<string> Dico = new List<string>();//
+            string[] lines = File.ReadAllLines("Mots_Français.txt");//           
+            Console.WriteLine(toString());//
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(' ');
+                Dico.AddRange(words);
+            }
+            Tri_XX(Dico);
+        }
+
+
+        //méthode toString : 
+        public static string toString()
+        {
+            string s = "Dans ce dictionnaire de langue française, il y a ";
+            string[] lines = File.ReadAllLines("Mots_Français.txt");
+            int[] tab = new int[16];
+            foreach (string line in lines)
+            {
+
+                string[] words = line.Split(' ');
+                foreach (string word in words)
+                {
+                    int a = word.Length;
+
+                    tab[a] += 1;
+                }
+            }
+            s += tab[1] + " mots avec 1 lettre, ";
+            for (int i = 2; i < 16; i++)
+            {
+                s += tab[i] + " mots avec " + i + " lettres, ";
+            }
+            s += " et aucun mot avec plus de lettres.";
+
+            return s;
+        }
+        //méthodes pour l'algorithme de tri : 
+        static void Tri_XX(List<string> liste)
+        {
+            if (liste.Count <= 1)
+                return;
+
+            int milieu = liste.Count / 2;
+            List<string> gauche = liste.GetRange(0, milieu);
+            List<string> droite = liste.GetRange(milieu, liste.Count - milieu);
+
+            Tri_XX(gauche);
+            Tri_XX(droite);
+
+            Fusionner(liste, gauche, droite);
+        }
+        static void Fusionner(List<string> liste, List<string> gauche, List<string> droite)
+        {
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            while (i < gauche.Count && j < droite.Count)
+            {
+                // Utilisation de CompareTo pour comparer les chaînes, basé sur l'ordre des lettres dans l'alphabet
+                if (gauche[i].CompareTo(droite[j]) < 0)
+                    liste[k++] = gauche[i++];
+                else
+                    liste[k++] = droite[j++];
+            }
+            while (i < gauche.Count)
+            {
+                liste[k++] = gauche[i++];
+            }
+            while (j < droite.Count)
+            {
+                liste[k++] = droite[j++];
+            }
+
+        }
+        //méthode de vérification du mot
+        static bool verification(string mot, List<string> Dico)
+        {
+            bool rep = false;
+            foreach (string word in Dico)
+            {
+                //Console.WriteLine(word);
+                if (word == mot)
+                {
+                    rep = true;
+                    break;
+                }
+            }
+            return rep;
+        }
     }
 }
--- Joueur.cs
+++ Joueur.cs
@@ -7,6 +7,46 @@ using System.Threading.Tasks;
 namespace ProjetInfoMotsCroises
 {
     internal class Joueur
+
     {
+        public List<string> mots = new List<string>();
+        public int score = 0;
+        public string nom;
+        public void Add_Mot(string mot)
+        {
+            if (mots.Contains(mot) == false)
+            {
+                mots.Add(mot);
+            }
+
+        }
+        public bool Contient(string mot)
+        {
+
+            for (int i = 0; i < mots.Count; i++)
+            {
+                if (mots[i] == mot)
+                {
+                    return true;
+                    break;
+                }
+            }
+            return false;
+        }
+        public string toString()
+        {
+            string description = "nom du joueur : " + nom + "\nmotstrouvés : ";
+            for (int i = 0; i < mots.Count; i++)
+            {
+                description += mots[i] + "\n";
+            }
+            description += "score : " + score + "\n";
+            return description;
+        }
+        public void Add_Score(int val)
+        {
+            score += val;
+
+        }
     }
 }
