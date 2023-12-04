7358dc2a53d877e6e04bf2f485e0730b65f68d17
PaserChess <150555513+PaserChess@users.noreply.github.com>
2023-12-04 22:49:28 +0100

    changement du programme, tout est bien là

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            //ici, on déclare tout, les noms des joueurs, et l'heure pour suivre le temps
             Jeu InstanceDeJeu = new Jeu();
             Joueur joueur1 = new Joueur();
             Joueur joueur2 = new Joueur();
@@ -15,12 +16,19 @@
             int temps0 = dateTime.Minute;
             while(DateTime.Now.Minute <temps0+5)
             {
+                //tour du joueur 1saisie du mot
                 Console.WriteLine("Joueur 1, saisissez votre mot : ");
                 string motj1 = Console.ReadLine();
+                //Vérification du mot dans le dictionnaire
+                //     Malo    il faudra faire la vérification dans le plateau avec un "&&"
                 if (InstanceDeJeu.Dico.RechercheDichoRecursif(0, 120000,  , motj1)==true)
                 {
                     joueur1.mots.Add(motj1);
                     Console.WriteLine("Bien joué ! ");
+                    joueur1.score += motj1.Length;
+                    Console.WriteLine("Le score de " + joueur1.nom + " est de " + joueur1.score);
+                    Console.WriteLine("Voici le tableau après ce tour : ");
+                    Plateau AffichageJeu = new Plateau("Test");
                 }
                 else
                 {
@@ -28,15 +36,23 @@
                 }
                 Console.WriteLine("Joueur 2, saisissez votre mot : ");
                 string motj2 = Console.ReadLine();
+
+
                 if (InstanceDeJeu.Dico.RechercheDichoRecursif(0, 120000,  , motj2) == true)
                 {
-                    joueur1.mots.Add(motj2);
+                    joueur2.mots.Add(motj2);
                     Console.WriteLine("Bien joué ! ");
+                    joueur2.score += motj2.Length;
+                    Console.WriteLine("Le score de " + joueur2.nom + " est de " + joueur2.score);
+                    Console.WriteLine("Voici le tableau après ce tour : ");
+                    Plateau AffichageJeu=new Plateau("Test");
                 }
                 else
                 {
                     Console.WriteLine("Ce mot n'existe pas, vous avez perdu la main ");
                 }
+
+                
             }
 
 
